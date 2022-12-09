a02257cab522cdf8008061fdd9a2c21131ba098f
Dan Stewart <git@mail.danstewart.dev>
2022-12-09 12:02:41 +0000

    Style tweaks
    - Tweak code colour
    - Fix markdown header width
    - Save theme when changed

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,16 +1,35 @@
 // Toggle between light and dark theme
-const toggleTheme = () => {
+const setTheme = to => {
 	const SUN = `<ion-icon name="sunny-outline"></ion-icon>`;
 	const MOON = `<ion-icon name="moon-outline"></ion-icon>`;
 
-	document.body.classList.toggle("light");
+	if (to === "light") {
+		document.body.classList.add("light");
+	} else {
+		document.body.classList.remove("light");
+	}
 
-	let isLight = document.body.classList.contains("light");
-
+	let isLight = to === "light";
 	let anchor = document.querySelector("#theme-toggle");
 	anchor.innerHTML = isLight ? MOON : SUN;
+	window.localStorage.setItem("theme", to);
+};
+
+// Get the current theme
+const getCurrentTheme = () => {
+	let currentTheme = "dark";
+	if (window.localStorage.getItem("theme") === "light") {
+		currentTheme = "light";
+	}
+
+	return currentTheme;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	document.querySelector("#theme-toggle").addEventListener("click", () => toggleTheme());
+	setTheme(getCurrentTheme());
+
+	document.querySelector("#theme-toggle").addEventListener("click", () => {
+		const switchTo = getCurrentTheme() === "light" ? "dark" : "light";
+		setTheme(switchTo);
+	});
 });
